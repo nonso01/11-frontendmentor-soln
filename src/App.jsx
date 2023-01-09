b73d2876aa55bb447568b672560ae3738ae7fb66
nonso01 <chukwunonsomartin01@gmail.com>
2023-01-09 14:34:49 +0100

    working on components

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
 import { useState } from "react";
+import Header from "./components/Header";
 import Attr from "./components/Attr";
 import "../dist/main.min.css";
 
 function App() {
   let [count, setCount] = useState(0);
 
-  function increaseCount() {
-    setCount(count + 1);
-  }
-
-  function decreaseCount() {
-    setCount(count - 1);
-
-    if (count <= 0) setCount((count = 0));
-  }
-
-  function resetCount() {
-    setCount((count = 0));
-  }
-
   return (
-    <div>
-      <h1>hello react js {count}</h1>
-      <button onClick={increaseCount}> count++ </button>
-      <button onClick={decreaseCount}> count-- </button>
-      <button onClick={resetCount}> count = 0 </button>
-
-      {/* <Attr></Attr> */}
+    <div id="app">
+      <Header />
     </div>
   );
 }
